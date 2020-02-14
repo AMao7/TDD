e94292ee687bd68ecb847e364063cf3f39cbb34d
AMao7 <abdimalikmao7@gmail.com>
2020-02-14 09:59:40 +0000

    TDD and unit testing

---
+++ TDD.py
@@ -0,0 +1,5 @@
+# Pros for TDD = Reduce technical depth, follows Agile framework (working software), visibility on how well the code
+# performing and when we are done. Creates a more maintanable code
+
+# Unit testing is when you take a piece of code and test them
+# Functions should be small and functional
